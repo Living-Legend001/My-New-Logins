0b90c80305655ef18c6473dd401d06884bc51632
Living-Legend001 <165025099+Living-Legend001@users.noreply.github.com>
2024-11-02 07:18:42 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,23 @@
+alert('Hello, Welcome');
+console.log('submit-button')
+
+function emailValidate(email) {
+	return email.includes('@');
+}
+function clickListener(event) {
+	event.preventDefault();
+	let emailInput = document.querySelector('#email');
+	let messageInput = document.querySelector('#message');
+
+	let emailText = emailInput.value;
+	let messsageText = message.value;
+	
+	if(emailValidate(emailText) !==true) {
+
+
+	console.log('The email address must contain @');
+		return false;
+	}
+	console.log('Thanks for your message');
+}
+submitButton.addEventListener('click', clickListener);
